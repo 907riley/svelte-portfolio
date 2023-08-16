c42b55fe4a795563ac5059d9120117a8e29be0aa
907riley <907riley@gmail.com>
2023-08-16 16:23:51 -0700

    working on resume page

--- portfolio/src/pages/Projects.svelte
+++ portfolio/src/pages/Projects.svelte
@@ -1,35 +1,113 @@
 <div>
     <div class="content-wrapper">
-        <h1>Dunk</h1>
+        <div>
+            <h1>Dunk</h1>
+            <p class="worked-on-wrapper">August 2023</p>
+        </div>
         <div class="images-wrapper">
             <img src='/dunk_main_menu.png' alt='dunk main menu scene'/>
             <img src='/dunk_gameplay.png' alt='dunk gameplay'/>
         </div>
+        <a href='https://granitebouldergames.itch.io/dunk'>Itch.io</a>
         <ul class="description-list-wrapper">
             <li>
-                The player must collect parts scattered around the ocean floor while managing oxygen levels, flashlight battery, and being hunted by a creature.
+                Built with Godot for a two week long game jam with a friend.
             </li>
             <li>
                 A first person 64 x 64 bit atmospheric horror game where the player plays as a diver stranded at the bottom of the ocean trying to fix their submarine.
             </li>
             <li>
-                Built with Godot for a two week long game jam with a friend.
+                The player must collect parts scattered around the ocean floor while managing oxygen levels, flashlight battery, and being hunted by a creature.
             </li>
         </ul>
-        <a href='https://granitebouldergames.itch.io/dunk'>Itch.io Page</a>
-        <div class="divider-line">
-
-        </div>
+        <div class="divider-line"></div>
     </div>
+
     <div class="content-wrapper">
-        <h1>Job Hunt Tracker</h1>
-        <p class="paragraph-wrapper">
-
-        </p>
+        <div>
+            <h1>Job Hunt Tracker</h1>
+            <p class="worked-on-wrapper">July 2023 - Present</p>
+        </div>
+        <div class="images-wrapper">
+            <img src='/job_hunt_tracker_view.PNG' alt='job hunt tracker main view'/>
+        </div>
+        <div class="link-wrapper">
+            <a href='https://application-tracker-vue.vercel.app/'>Website</a>
+            <a href="https://github.com/907riley/application-tracker-vue">GitHub</a>
+        </div>
+        <ul class="description-list-wrapper">
+            <li>
+                Vue.js and Typescript application that allows users to store and manage all the information about the jobs they apply to, organized into job hunts.
+            </li>
+            <li>
+                Uses supabase to handle CRUD along with user authentication via email.
+            </li>
+            <li>
+                Frontend designed with Figma and tested with Vitest.
+            </li>
+        </ul>
+        <div class="divider-line"></div>
     </div>
+
+    <div class="content-wrapper">
+        <div>
+            <h1>Jig</h1>
+            <p class="worked-on-wrapper">July 2023</p>
+        </div>
+        <div class="images-wrapper">
+            <img src='/jig.gif' alt='jig logo'/>
+            <img src='/jig_gameplay.png' alt='jig gameplay'/>
+        </div>
+        <a href='https://granitebouldergames.itch.io/jig'>Itch.io</a>
+        <ul class="description-list-wrapper">
+            <li>
+                Built with Godot for a 48 hour game jame with a friend. I made most of the artwork with Aseprite.
+            </li>
+            <li>
+                Created a pixel art rhythm fishing game that shuffled through random songs and difficulties.
+            </li>
+            <li>
+                Players were awarded 1 of 10 possbile fish based on how they performed.
+            </li>
+        </ul>
+        <div class="divider-line"></div>
+    </div>
+
+    <div class="content-wrapper">
+        <div>
+            <h1>Portfolio</h1>
+            <p class="worked-on-wrapper">May 2023 - Present</p>
+        </div>
+        <div class="images-wrapper">
+            <img id='svelte-logo-wrapper' src='/svelte_logo.png' alt='jig logo'/>
+        </div>
+        <p>This website</p>
+        <ul class="description-list-wrapper">
+            <li>
+                I've gone through 3-4 redesigns of this website recently. Originally no js at all, then redesigned with js, then remade with Next.js, and then finally remade with Svelte.
+            </li>
+            <li>
+                First time that I designed a website with a mobile first plan.
+            </li>
+        </ul>
+        <div class="divider-line"></div>
+    </div>
+
+
 </div>
 
 <style lang="scss">
+
+    #svelte-logo-wrapper {
+        width: 10rem;
+    }
+
+    .link-wrapper {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
 
     .description-list-wrapper {
         list-style-type: circle;
@@ -42,6 +120,7 @@
     .images-wrapper {
         display: flex;
         flex-direction: row;
+        gap: 1rem;
 
         img {
             // width: 100px;
--- portfolio/src/pages/Resume.svelte
+++ portfolio/src/pages/Resume.svelte
@@ -1,3 +1,5 @@
 <div>
-    Resume
+    <div class="content-wrapper">
+        <img src='/resume-8-16-2023.png' alt="My resume as of August 16th, 2023"/>
+    </div>
 </div>
