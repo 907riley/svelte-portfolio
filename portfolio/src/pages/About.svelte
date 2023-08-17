e56c9954fcb4b4909d2a56623f276ddcec6219d2
907riley <907riley@gmail.com>
2023-08-17 12:35:40 -0700

    updated about and image headshot settings

--- portfolio/src/pages/About.svelte
+++ portfolio/src/pages/About.svelte
@@ -2,32 +2,30 @@
     <div class="content-wrapper">
         <img id="headshot"  src="/cropped_grad_headshot.png" alt="Graduation Headshot"/>
         <p class="paragraph-wrapper">
-            I grew up in Juneau Alaska until moving to Spokane, WA to pursue a computer science degree at Gonzaga University. I attribute my interest in programming to my involvement in the FIRST robotics programs starting in elementary school, culminating in my senior year in high school where my team won our state competition to qualify for the worlds competition in Houston, TX. I was never the programmer in robotics so I always thought I wanted to be an engineer until I took an AP computer science course my senior year of high school. After taking that class I knew I wanted to learn more. 
+            I grew up in Juneau, AK until moving to Spokane, WA to pursue a computer science degree at Gonzaga University. I attribute my interest in programming to my involvement in the FIRST robotics programs starting in elementary school, culminating in my senior year in high school where my team won our state competition to qualify for the worlds competition in Houston, TX. I was never a programmer in robotics so I always thought I wanted to be an engineer until I took an AP computer science course my senior year of high school. After taking that class I knew I wanted to learn more. 
         </p>
         <p class="paragraph-wrapper">
-            I graduated May 2023 with a B.S in Computer Science along with a Minor in Mathematics. I&apos;m currently still living in Spokane, enjoying my free time working on some projects while I search for a job.
+            I graduated May 2023 with a B.S in Computer Science along with a Minor in Mathematics. I'm currently still living in Spokane, enjoying my free time working on some projects while I search for a job.
         </p>
     </div>
     <div class="content-wrapper">
         <img id="fisherman" src="fisherman_static.gif" alt="Animation of a pixel art fisherman"/>
         <p class="paragraph-wrapper">
-            My biggest interests when it comes to computer science are web development and game development. My senior design project that I spent my senior year at Gonzaga working on required me to learn and use React, which has now prompted me to re-make my website to what you are seeing now. I&apos;m a big fan of pixel art and absolutely love fishing games, so I decided to have a little fun with the design. I&apos;m also working on a rhythm fishing game with a friend from back home that we hope to have a working demo completed by the end of the summer. Some of my favorite games that are inspirations to me for this game are rock band, stardew valley, and dredge.
+            Full stack and game development are my two largest interests in regards to computer sciences. My senior design project at Gonzaga required me to learn React, which has now prompted me to remake my website. Although I have remade it again in Svelte because Svelte is awesome. I'm a big fan of pixel art and absolutely love fishing games (all the best games have a fishing minigame), so I decided to have a little fun with the design. Read about how I've incorporated these into some of my other projects on my projects page. 
         </p>
     </div>
     <div class="content-wrapper">
-        <p class="paragraph-wrapper">
-            Outside of programming, I spend a lot of my time playing ultimate frisbee, disc golfing, and in the gym. I recently joined a club Ultimate team in Spokane that I am stoked about and I&apos;ve been getting in some casual disc golf as a break from working on my projects.
-        </p>
-        <p class="paragraph-wrapper">
-            Here's a recent video of me bidding while playing ultimate
-        </p>
         <img id="laying-out" src="ultimate.gif" alt="Video of me playing ultimate"/>
+        <p class="image-caption">Actual footage of me bidding while playing ultimate</p>
+        <p class="paragraph-wrapper">
+            Outside of programming, I spend a lot of my time playing ultimate frisbee, disc golfing, and in the gym. I recently joined a club Ultimate team in Spokane that I am stoked about and I've been getting in some casual disc golf as a break from working on my projects. I also enjoy playing video games occasionally, but I haven't had much time lately for them. Some of my all time favorite games are Pokemon (specifically gen 4 and 5), Stardew Valley, Minecraft, Dredge, The Forest, and Rock Band.
+        </p>
     </div>
 </div>
 
 <style lang="scss">
     #headshot {
-        padding: 0px 32px 0px 32px;
+        width: 20rem;
     }
 
     #fisherman {
