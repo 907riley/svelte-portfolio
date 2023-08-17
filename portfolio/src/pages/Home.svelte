0b07e279ae4304ca41224a56cf647e54b1fd2bf8
907riley <907riley@gmail.com>
2023-08-17 13:06:48 -0700

    couple typo fixes

--- portfolio/src/pages/Home.svelte
+++ portfolio/src/pages/Home.svelte
@@ -5,13 +5,13 @@
         <p>907riley@gmail.com</p> 
     </div>
     <p class="paragraph-wrapper">
-        May 2023 I graduated college with a degree in computer science in a slight state of panic because I didn't have a job lined up. Throughout college, professors and advisors had told me how important it was to apply early and have something lined up right out of college. That there was a formula to getting a good paying job in this industry: get internships during the summers leading up to your senior year and that will result in a job post college. I got those internships and thoroughly enjoyed them, but they did not land me that job (I blame the current state of the job market, but that's beside the fact). So now what?      
+        May 2023 I graduated college with a degree in computer science in a slight state of panic because I didn't have a job lined up. Throughout college, professors and advisors had told me how important it was to apply early and have something lined up right out of college. That there was a formula to getting a good paying job in this industry: get internships during the summers leading up to your senior year and that will result in a job post college. I got those internships and thoroughly enjoyed them, but they did not land me that job. So now what?      
     </p>
     <p class="paragraph-wrapper">
         I decided that I wanted to dig into some personal projects that I had complete control over. I've been spending my time working on multiple fun projects ranging from websites to video games that you can check out on my projects page.
     </p>
     <p class="paragraph-wrapper">
-        This website looks and performs fine on mobile, but I highly recommend viewing my website on a laptop or desktop screen. I incorporated some pixel art into the larger screen designs that adds a lot of character to it.
+        This website looks and performs fine on mobile, but I highly recommend viewing it on a laptop or desktop screen. I incorporated some pixel art into the larger screen designs that adds a lot of character to it.
     </p>
 
 </div>
