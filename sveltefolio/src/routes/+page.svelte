88006cb9807ca16eedbc754b8102b3e9d456022f
907riley <907riley@gmail.com>
2023-10-14 09:30:21 -0700

    updates website home page for new job, cats, and game dev update

--- sveltefolio/src/routes/+page.svelte
+++ sveltefolio/src/routes/+page.svelte
@@ -5,9 +5,26 @@
         <p>907riley@gmail.com</p> 
     </div>
     <p class="paragraph-wrapper">
-        May 2023 I graduated college with a degree in computer science in a slight state of panic because I didn't have a job lined up. Throughout college, professors and advisors had told me how important it was to apply early and have something lined up right out of college. That there was a formula to getting a good paying job in this industry: get internships during the summers leading up to your senior year and that will result in a job post college. I got those internships and thoroughly enjoyed them, but they did not land me that job. So now what?      
+        Summer is over and the best part of the year is officially here: fall. I didn’t always have this opinion though, as fall growing up in Juneau consisted of non stop rain, wind warnings, and barely any leaves as most of the trees are coniferous. So when I first got to experience fall in Spokane (after the way too hot summers we get here), I was pleasantly surprised. Fall is also home to the best holiday: Halloween. I’m a big horror movie fan and there is no better time to watch some spooky movies than during fall in October.
     </p>
     <p class="paragraph-wrapper">
-        I decided that I wanted to dig into some personal projects that I had complete control over. I've been spending my time working on multiple fun projects ranging from websites to video games that you can check out on my projects page.
+        On top of it being the best time of the year, I finally got a job. I accepted a job as a Systems Test Engineer at Itron where I’ve been learning how to work with different IoT devices. There’s definitely a learning curve with the size of the solution, but I will get to learn a lot about networking which is awesome. So far I’ve been really enjoying it and I’m very grateful for the opportunity at a time when the job market seems so dull. I also just got two kittens which has been so fun. They don’t always make the best decision, like sneaking up behind and jumping in the toilet when I’m going to the bathroom or coming to the conclusion that my bare legs would make good climbing material, but we still love them.
+    </p>
+    <img id="sleepy-cats" src="cats_sleepy.jpeg" alt="sleepy cats"/>
+    <p class="image-caption">They sleep a lot</p>
+    <p class="paragraph-wrapper">
+        In terms of game development, I’m currently working on a small and large project. I was originally just working on my large project, but I decided that I should try to make a smaller game to get more comfortable with Godot and the game development project before diving head first into a multi-year long project. The large project (which I’m purposely not mentioning the idea yet), is a game that I view as my perfect game that combines aspects of all my favorite games. It’s definitely become a victim to a bit of scope creep and I’ve done my best to restrain that, but I think it's best that I give that idea more time to simmer. The smaller project on the other hand is a game that I’m calling Show Off for now. The idea of the game is a 3rd person speed climbing game where the player has to control just the characters hands and arms to climb a rock wall as fast as they can. The player won’t be able to use the characters legs because they’re a bit of a show off.
+    </p>
+    <img id="title-drawing" src="show_off_title_drawing.jpeg" alt="initial title drawing of show off"/>
+    <p class="image-caption">An initial mock up I did of the title screen for Show Off</p>
+    <p class="paragraph-wrapper">
+        I’m going to be doing some dev logs under the Projects/Current pages, so go read about it to learn what I’ve been working on.
     </p>
 </div>
+
+<style lang="scss">
+    #sleepy-cats,
+    #title-drawing {
+        width: 20rem;
+    }
+</style>
